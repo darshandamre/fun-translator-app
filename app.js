d63334fabce788b9bc4786174420e200df66baad
darshan <darshandamre5@gmail.com>
2021-08-13 01:43:24 +0530

    done with js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("hey banananana");
+const input = document.getElementById("input");
+const button = document.querySelector(".btn");
+const output = document.getElementById("output");
+
+const url = "https://api.funtranslations.com/translate/minion.json";
+
+button.addEventListener("click", () => {
+  fetch(url + "?text=" + input.value)
+    .then((response) => response.json())
+    .then((json) => (output.innerText = json.contents.translated))
+    .catch((err) => console.log("Error: " + err));
+});
